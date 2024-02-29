b7892d4105e0f2d411bfb324e2959c4199030366
viryasov1 <95222958+viryasov1@users.noreply.github.com>
2024-02-29 11:07:39 +0300

    Add files via upload

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Wpf.Classes;
+using Wpf.Pages;
+
+namespace Wpf
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            Manager.MainFrame = MainWindowFrame;
+            Manager.MainFrame.Navigate(content: new AuthPage());
+        }
+        private void MainWindowFrame_ContentRendered(object sender, EventArgs e)
+        {
+        }
+        private void BtnBack_Click(object sender, RoutedEventArgs e)
+        {
+        }
+    }
+}
